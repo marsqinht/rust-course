81e6eba68f8e10a2eb9cd4dad11d0063f317fc92
qinhaitao <qinhaitaocn@163.com>
2023-03-13 15:41:26 +0800

    feat:variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,42 @@
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     let _x = 5;
+//     let y = 10;
+// }
+
+// fn main() {
+//     let (a, mut b): (bool,bool) = (true, false);
+//     // a = true,不可变; b = false，可变
+//     println!("a = {:?}, b = {:?}", a, b);
+
+//     b = true;
+//     assert_eq!(a, b);
+// }
+
+struct Struct {
+    e: i32
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (a, b, c, d, e);
+
+    (a, b) = (1, 2);
+    // _ 代表匹配一个值，但是我们不关心具体的值是什么，因此没有使用一个变量名而是使用了 _
+    [c, .., d, _] = [1, 2, 3, 4, 5];
+    Struct { e, .. } = Struct { e: 5 };
+
+    assert_eq!([1, 2, 1, 4, 5], [a, b, c, d, e]);
 }
