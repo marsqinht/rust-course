874847c19e3e9e7bef1133f3a82aabc64fcbe4b8
qinhaitao <qinhaitaocn@163.com>
2023-03-14 23:18:56 +0800

    feat: array

--- union-type/src/main.rs
+++ union-type/src/main.rs
@@ -4,14 +4,55 @@
 // }
 
 // 🌟🌟 如果要使用 str 类型，只能配合 Box。 & 可以用来将 Box<str> 转换为 &str 类型
-fn main() {
-    let s: Box<str> = "hello, world".into();
-    greetings(s)
-}
+// fn main() {
+//     let s: Box<str> = "hello, world".into();
+//     greetings(s)
+// }
 
-// fn greetings(s: &str) {
+// // fn greetings(s: &str) {
+// //     println!("{}",s)
+// // }
+// fn greetings(s: str) {
 //     println!("{}",s)
 // }
-fn greetings(s: str) {
-    println!("{}",s)
+
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!("rect1 is {:?}", rect1);
+// }
+
+use std::io;
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+    // 读取控制台的输出
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
