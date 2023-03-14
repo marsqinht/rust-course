f60950af6bf4968a467e4b03771785abe14ff839
qinhaitao <qinhaitaocn@163.com>
2023-03-14 19:23:11 +0800

    feat: str practice

--- union-type/src/main.rs
+++ union-type/src/main.rs
@@ -1,9 +1,17 @@
+// 1.🌟 正常情况下我们无法使用 str 类型，但是可以使用 &str 来替代
+// fn main() {
+//     let _s: &str = "hello, world";
+// }
+
+// 🌟🌟 如果要使用 str 类型，只能配合 Box。 & 可以用来将 Box<str> 转换为 &str 类型
 fn main() {
-    let my_name = "Pascal";
-    greet(my_name);
-  }
-  
-fn greet(name: &str) {
-println!("Hello, {}!", name);
+    let s: Box<str> = "hello, world".into();
+    greetings(s)
 }
-  
+
+// fn greetings(s: &str) {
+//     println!("{}",s)
+// }
+fn greetings(s: str) {
+    println!("{}",s)
+}
