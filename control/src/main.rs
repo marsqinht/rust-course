9602a5bf330917206a6b6473bb88380b0927b2f1
qinhaitao <qinhaitaocn@163.com>
2023-03-15 10:51:30 +0800

    feat: for in

---
+++ control/src/main.rs
@@ -0,0 +1,16 @@
+// fn main() {
+//     let condition = true;
+//     let number = if condition {
+//         5
+//     } else {
+//         6
+//     };
+
+//     println!("The value of number is: {}", number);
+// }
+
+fn main() {
+  for i in 1..=5 {
+    println!("{}", i)
+  }
+}
